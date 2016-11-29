be1eb1ed04dec9b7a95af5984a16697acd4a84fb
Rosie <r.gollancz@gmail.com>
2016-11-29 16:34:23 +0000

    Update table for larger amounts

--- lib/statement.rb
+++ lib/statement.rb
@@ -8,16 +8,16 @@ module Statement
 
   def format_amount(amount)
     a = "#{"%.2f" % amount.abs}"
-    debit_space = "        "
-    space_after = " " * (7-(a.length))
+    debit_space = "            "
+    space_after = " " * (11-(a.length))
     amount >0 ? (" #{a}" + space_after + "||")  : (debit_space + "|| #{a}" + space_after)
   end
 
   def format_balance(amount, balance)
-    amount >0 ? "         || #{"%.2f" % balance}" : " || #{"%.2f" % balance}"
+    amount >0 ? "             || #{"%.2f" % balance}" : " || #{"%.2f" % balance}"
   end
 
   def statement_headers
-    "date       || credit || debit   || balance"
+    "date       || credit     || debit       || balance"
   end
 end
